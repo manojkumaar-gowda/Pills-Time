ee98e4d8f4d1a6b57a5b7e02bd9b5f4d817e0f5a
ManojKumaar R <manojkumaar.1905047>
2022-05-23 10:32:15 +0530

    Added Splash Screen

---
+++ app/src/main/java/com/huawei/batch6/pillstime/SplashScreenActivity.java
@@ -0,0 +1,24 @@
+package com.huawei.batch6.pillstime;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+//Redirects to the login Page after 2.3 seconds
+        final Handler handler = new Handler();
+        handler.postDelayed(() -> {
+            Intent send = new Intent(getApplicationContext(),
+                    MainActivity.class);
+            startActivity(send);
+            finish();
+        }, 2300);
+    }
+}
