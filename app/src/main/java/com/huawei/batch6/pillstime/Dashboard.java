ede75a6a6d78e8bc2e2bafd2f9414a15bf5ad24c
ManojKumaar R <manojkumaar.1905047>
2022-05-25 21:53:05 +0530

    Added floating button

--- app/src/main/java/com/huawei/batch6/pillstime/Dashboard.java
+++ app/src/main/java/com/huawei/batch6/pillstime/Dashboard.java
@@ -4,7 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
 public class Dashboard extends AppCompatActivity {
 
@@ -14,6 +18,14 @@ public class Dashboard extends AppCompatActivity {
         setContentView(R.layout.activity_dashboard);
         TextView regButton = findViewById(R.id.medical_sugestions);
         regButton.setOnClickListener(view -> regFunction());
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Here's a Snackbar", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
     }
     public void regFunction(){
         Intent send = new Intent(Dashboard.this, MedicalSuggestions.class);
